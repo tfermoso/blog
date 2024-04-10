5a246b7bdae390cd85bde038f6a53305357aee05
tfermoso <tomas.fermoso@gmail.com>
2024-04-10 20:41:22 +0200

    creando Project Model

--- blog/apps.py
+++ blog/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
+
+class ProjectConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'project'
--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,24 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Project (models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    image = models.ImageField(upload_to='img/',default='img/logo.png')
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
